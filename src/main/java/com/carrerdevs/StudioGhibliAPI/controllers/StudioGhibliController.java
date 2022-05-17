a272bebe6320acf1b6c83d19ac04c4d3a2586edb
PierreEdFils <epierrefils@gmail.com>
2022-05-17 14:50:18 -0400

    Initial commit

---
+++ src/main/java/com/carrerdevs/StudioGhibliAPI/controllers/StudioGhibliController.java
@@ -0,0 +1,25 @@
+package com.carrerdevs.StudioGhibliAPI.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ghibli")
+
+public class StudioGhibliController {
+//    @Autowired
+//    Environment environment;
+//
+//    private String myStudioKey = "DEMO_KEY";
+//
+//    private String nasaApodEndpoint = "https://ghibliapi.herokuapp.com/apod?api_key=" + myStudioKey;
+//
+//    @GetMapping ("/films")
+//    public String getFilm (){
+//        return environment.getProperty(myStudioKey);
+//    }
+
+}
